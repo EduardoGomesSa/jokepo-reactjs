600c51c9002ccbb7803b73a362a08b0148151ee7
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-02-07 23:10:49 -0300

    criação da estrutura básica do css do layout

--- arquivos/src/components/Footer.jsx
+++ arquivos/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import styles from '../styles/Footer.module.css'
 export default function Footer(){
     return (
         <div className={styles.footer}>
-            <h1>Aqui ficará o rodapé</h1>
+            <p>&copy;Eduardo - 2023</p>
         </div>
     )
 }
--- arquivos/src/components/Header.jsx
+++ arquivos/src/components/Header.jsx
@@ -3,7 +3,8 @@ import styles from '../styles/Header.module.css'
 export default function Header(){
     return (
         <div className={styles.header}>
-            <h1>Aqui ficará o cabeçalho</h1>
+            <h1>Jo-Ke-Po</h1>
+            <h2>Tente a solte!</h2>
         </div>
     )
 }
