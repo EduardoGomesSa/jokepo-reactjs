6601f471b2cbcd4cadfb3fcdf8d2cf7926d2adba
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-07-15 23:57:00 -0300

    criação da div do component MoveSelected

--- src/components/game/MoveSelected.tsx
+++ src/components/game/MoveSelected.tsx
@@ -1,3 +1,12 @@
-export default function MoveSelected(){
-    return ""
+interface MoveSelectedProps{
+    imgAddress: string
+    onChange?: (address: string) => void
+}
+
+export default function MoveSelected(props: MoveSelectedProps){
+    return (
+        <div className={styles.moveSelected}>
+            <img src={props.imgAddress} alt="imagem da jogada escolhida pelo usuario"/>
+        </div>
+    )
 }
